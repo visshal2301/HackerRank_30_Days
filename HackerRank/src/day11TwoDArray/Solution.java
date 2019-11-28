c5606e32efff0ce2843f0bf43490202e9c218517
Akshay <Akshay@DESKTOP-03IPIVK>
2019-11-28 16:56:10 +0530

    Commit

--- HackerRank/src/day11TwoDArray/Solution.java
+++ HackerRank/src/day11TwoDArray/Solution.java
@@ -28,7 +28,7 @@ public class Solution {
                     int sum = arr[i][j] + arr[i][j + 1] + arr[i][j + 2] + arr[i + 1][j + 1] + arr[i + 2][j] + arr[i + 2][j + 1] + arr[i + 2][j + 2];
                     if (sum > max) max = sum;
                 }
-            }
+               }
         }
 
         System.out.println(max);
